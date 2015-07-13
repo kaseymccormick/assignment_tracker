7e26b0b51465ebf66aaee1dc3422de73bf393f0b
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-13 18:28:21 -0500

    renaming

--- database_setup.rb
+++ database_setup.rb
@@ -3,9 +3,9 @@ CONNECTION = SQLite3::Database.new("assignments.db")
 
 # Make the tables.
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS assignments (id INTEGER PRIMARY KEY, description TEXT, git_repo TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS resource_types (id INTEGER PRIMARY KEY, category TEXT);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS resource_categories (id INTEGER PRIMARY KEY, catigory TEXT);")
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS resources (id INTEGER PRIMARY KEY, rt_id INTEGER, assignment_id INTEGER);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS contributions (user_id INTEGER, assignment_id INTEGER);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS contributions (contributor_id INTEGER, assignment_id INTEGER);")
 CONNECTION.execute("CREATE TABLE IF NOT EXISTS contributors (id INTEGER PRIMARY KEY, name TEXT);")
 
 # Get results as an Array of Hashes.
