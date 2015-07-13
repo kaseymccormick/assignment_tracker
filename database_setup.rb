cf1d81d6e4600329f380d42aee9e43d173b2b491
Kasey McCormick <mccormick.kasey@gmail.com>
2015-07-13 12:56:47 -0500

    file structure and naming

--- database_setup.rb
+++ database_setup.rb
@@ -1,12 +1,12 @@
 # Load/create our database for this program.
-CONNECTION = SQLite3::Database.new("availability_overlap.db")
+CONNECTION = SQLite3::Database.new("assignments.db")
 
 # Make the tables.
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS responders (id INTEGER PRIMARY KEY, name TEXT,e_mail TEXT, age_id INTEGER);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS ages (id INTEGER PRIMARY KEY, range TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS time_frames (id INTEGER PRIMARY KEY, slot TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS days (id INTEGER PRIMARY KEY, date TEXT);")
-CONNECTION.execute("CREATE TABLE IF NOT EXISTS available_times (responders_id INTEGER, timeframes_id INTEGER, days_id INTEGER);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS assignments (id INTEGER PRIMARY KEY, description TEXT, github_repo TEXT);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS resource_types (id INTEGER PRIMARY KEY, category TEXT);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS resources (id INTEGER PRIMARY KEY, rt_id INTEGER);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS contributions (user_id INTEGER, assignment_id INTEGER);")
+CONNECTION.execute("CREATE TABLE IF NOT EXISTS contributors (id INTEGER PRIMARY KEY, name TEXT);")
 
 # Get results as an Array of Hashes.
 CONNECTION.results_as_hash = true
